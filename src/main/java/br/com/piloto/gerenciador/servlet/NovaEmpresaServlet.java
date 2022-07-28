5940d5a44fc9a55e90069adcba35c7d937923584
William Campos <william.c.campos@icloud.com>
2022-07-27 21:04:45 -0300

    Criacao form empresa

--- src/main/java/br/com/piloto/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/piloto/gerenciador/servlet/NovaEmpresaServlet.java
@@ -16,15 +16,16 @@ import jakarta.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public NovaEmpresaServlet() {
+	public NovaEmpresaServlet() {
 
-    }
+	}
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("Cadastro de nova empresa");
-		
+
 		String nomeEmpresa = request.getParameter("nome");
-		
+
 		PrintWriter out = response.getWriter();
 		out.print("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
 
